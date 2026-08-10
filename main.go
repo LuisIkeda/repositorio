84a40147f93d88430e5f9016debb7eee4046710c
luis.ikeda <ikeda.francis.luis@gmail.com>
2026-08-10 08:09:35 -0300

    correção xpto v1.2

--- main.go
+++ main.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main(){
     fmt.Println("Desenvolvimento")
-    fmt.Println("Feature Xpto v.1.2")
+    fmt.Println("Feature Xpto v.1.2 corrigida")
+    fmt.Println("hotfix Xpto v.1.2 corrigida")
 }
